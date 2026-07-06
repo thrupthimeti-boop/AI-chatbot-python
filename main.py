69f40947d8147f704df09430d94ff068a2f1ca75
Thrupthi S Meti <thrupthimeti@gmail.com>
2026-07-06 11:57:04 +0530

    Add chatbot welcome screen

---
+++ main.py
@@ -0,0 +1,7 @@
+print("===================================")
+print("        AI Chatbot in Python")
+print("===================================")
+
+print("Hello! I am Chintu 🤖")
+print("Welcome to my AI Chatbot.")
+print("Type 'bye' anytime to exit.")
